5137c4864e8ecf2f40d3a8ae1a0e6c619d1d95d6
Harry <133057991+harharry@users.noreply.github.com>
2023-05-29 22:50:04 -0400

    Update StudentTuesday4.java

--- src/studenttuesday4/StudentTuesday4.java
+++ src/studenttuesday4/StudentTuesday4.java
@@ -11,7 +11,7 @@ public class StudentTuesday4{
     
     public static void main(String[] args){
         
-        Student[] students = new Student[7]; //I changed the array size to 7
+        Student[] students = new Student[8]; //I changed the array size to 8
         
         Scanner keyboard = new Scanner(System.in);
         
